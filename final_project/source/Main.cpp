4c12d229ece462cac9c486d73495068eaa3d4189
Kevinkiller1024 <Kevinkiller9487@gmail.com>
2024-12-18 15:33:29 +0800

    Start programming

---
+++ final_project/source/Main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int display();
+int shuffle();
+int rull();
+
+
+int main() {
+	srand(time(NULL));
+	
+
+	
+	system("pause");
+	return 0;
+}
+
+int display() {
+
+}
+
+int shuffle() {
+
+}
+
+int rull() {
+
+}
