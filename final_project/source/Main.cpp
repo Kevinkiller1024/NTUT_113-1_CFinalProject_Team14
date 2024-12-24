b7fb59daaec0f89b5d539cca29814e456154634b
rayyu0826 <hyu32084@gmail.com>
2024-12-24 15:06:32 +0800

    2

--- final_project/source/Main.cpp
+++ final_project/source/Main.cpp
@@ -24,6 +24,21 @@ int shuffle() {
 
 }
 
-int rull() {
+int rull(int low, int high, int playerGuess) {
+    if (low > high) {
+        // 確保 low 小於或等於 high
+        int temp = low;
+        low = high;
+        high = temp;
+    }
 
+    // 判斷輸贏
+    if (playerGuess >= low && playerGuess <= high) {
+        return 1; // 玩家贏了
+    }
+    else {
+        return 0; // 玩家輸了
+    }
 }
+
+        
